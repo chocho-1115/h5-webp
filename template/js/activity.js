483e0f324af9ed077bd2936eafbb0e306b613d32
chocho <849890769@qq.com>
2025-03-13 18:00:28 +0800

    feat: 背景音乐功能抽离

--- template/js/activity.js
+++ template/js/activity.js
@@ -2,6 +2,8 @@
 import http from '../common/http.js'
 import {isWechat, isAndroid} from '../common/utils.js'
 
+import audio from './audio.js'
+
 document.body.ondragstart = function (e) {
     e.preventDefault()
 }
@@ -71,14 +73,14 @@ export default {
     
         if(isAndroid()){ // 安卓谷歌浏览器也无法自动播放 只能解决安卓微信 和默认浏览器的自动播放
             const res = await http.get(src, { responseType: 'arraybuffer' }).catch((e) => { console.log(e) })
-            audioConfig.audioContext = await this.createAudioContext(res.data)
+            audioConfig.audioContext = await audio.createContext(res.data)
         }
     
-        const audio = this.createAudio(audioConfig)
+        const m = audio.create(audioConfig)
     
-        this.setAudioButton({
+        audio.setButton({
             button: document.getElementById('micBtn'),
-            audio,
+            audio: m,
         })
     
         // 以下方式都能解决ios微信下音频自动播放的问题
@@ -86,128 +88,15 @@ export default {
             if(typeof window.WeixinJSBridge == 'object' && typeof window.WeixinJSBridge.invoke == 'function') {  
                 window.WeixinJSBridge.invoke('getNetworkType', {}, () => {
                     console.log('getNetworkType')
-                    audio.play() 
+                    m.play() 
                 })
             } else {
                 document.addEventListener('WeixinJSBridgeReady', function() {  
                     console.log('WeixinJSBridgeReady')
-                    audio.play()
+                    m.play()
                 }, false)  
             }  
         }
-    },
-    /**
-     * @desc 利用AudioContext api来播放音频
-     * @param {arraybuffer} arraybuffer 通过接口请求的音频文件数据
-     */
-    async createAudioContext (arraybuffer) {
-        const mgr = new Proxy({
-            context: new (window.AudioContext || window.webkitAudioContext || window.mozAudioContext)(),
-            autoplay: false,
-            paused: true,
-            loop: false,
-            pause () { mgr.context.suspend() },
-            play () { mgr.context.resume() },
-            _events: {},
-            trigger: function(eventName, ...args) {
-                if (!this._events[eventName]) return
-                this._events[eventName].forEach(handler => {
-                    handler.apply(this, args)
-                })
-            },
-            addEventListener(eventName, handler) {
-                if (!this._events[eventName]) {
-                    this._events[eventName] = []
-                }
-                this._events[eventName].push(handler)
-            },
-            removeEventListener: function(eventName, handler) {
-                if (!this._events[eventName]) return
-                this._events[eventName] = this._events[eventName].filter(h => h !== handler)
-            }
-        }, {
-            // get: function (target, propKey, receiver) {
-            //   console.log(`getting ${propKey}!`);
-            //   return Reflect.get(target, propKey, receiver);
-            // },
-            set: function (target, propKey, value, receiver) {
-                switch(propKey) {
-                case 'loop': source.loop = value; break
-                case 'autoplay': if(value===true) mgr.play(); break
-                }
-                return Reflect.set(target, propKey, value, receiver) // 同步修改 mgr[propKey]
-            }
-        })
-
-        mgr.context.addEventListener('statechange', function(e){
-            if(e.currentTarget.state == 'running'){
-                mgr.paused = false
-                mgr.trigger('play', {target: mgr, type: 'play'})
-            }else{
-                mgr.paused = true
-                mgr.trigger('pause', {target: mgr, type: 'pause'})
-            }
-        }, false)
-        
-        const buffer = await mgr.context.decodeAudioData(arraybuffer)
-        let source = mgr.context.createBufferSource()
-        source.buffer = buffer
-        source.loop = false
-        source.start(0)
-        source.connect(mgr.context.destination)
-        mgr.pause()
-        
-        return mgr
-    },
-    createAudio ({src = '', audioContext = null, autoplay = true, loop = true, asPossibleAutoplay = false}) {
-        if(!src && !audioContext) return
-        const audio = audioContext ? audioContext : new Audio(src)
-        audio.loop = !!loop
-        audio.autoplay = !!autoplay
-        
-        const firstTouch = (e) => {
-            document.removeEventListener(e.type, firstTouch)
-            if(audio.button && audio.button.contains(e.target)) return
-            audio.play()
-        }
-        if (autoplay && asPossibleAutoplay) {
-            audio.play()
-            if(audio.paused) {
-                // ios下 audioContext api时 touchstart事件会触发 但是play播放不成功，所以要加click来触发播放
-                // ios微信下 audioContext api时 空白处点击 无法触发window的click事件，所以这里的事件需绑定在document上，如果点击了注册事件的按钮上时 还是会冒泡触发window的click事件的
-                // 这里的事件如果有调整 需要综合考虑播放按钮的点击事件 考虑多事件注册后 在不同场景下的触发顺序
-                document.addEventListener('touchstart', firstTouch, false) 
-                document.addEventListener('click', firstTouch, false)
-            }
-        }
-        return audio
-    },
-    // 设置mp3 背景音乐按钮	
-    setAudioButton ({button = null, audio = null}) {
-        if(!audio || !button) return
-
-        audio.button = button
-
-        // 播放完毕
-        // audio.addEventListener('ended',function(){
-        //     button.classList.add('hide')
-        // },false)
-        // 暂停
-        audio.addEventListener('pause',function(){
-            button.classList.add('hide')
-        }, false)
-        // 播放
-        audio.addEventListener('play',function(){
-            button.classList.remove('hide')
-        }, false) 
-        
-        button.style.display = 'block'
-        button.addEventListener('click', () => {
-            audio.paused ? audio.play() : audio.pause()
-        })
-
-        if(audio.paused) button.classList.add('hide')
-        
     },
     // 设置省市区联动
     initHunanAreaPicker: function({city, area, district, depth, defaultValue = []} = {}, callback){
