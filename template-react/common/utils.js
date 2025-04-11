0e35cbde3cbd8503f372db92df84aa409127a873
chocho <849890769@qq.com>
2025-04-11 11:04:01 +0800

    feat: 更新utils.js

--- template-react/common/utils.js
+++ template-react/common/utils.js
@@ -258,97 +258,6 @@ export function isWechat(includePc) {
     return !!isWechat
 }
 
-// 预载器
-export function preload(srcArr, params) {
-    if (typeof (srcArr) == 'string') {
-        srcArr = [{ path: srcArr }]
-    };
-    if (srcArr.length == 0) { params.complete && params.complete({}); return false };
-    let num = 0,
-        imgArrObj = {},
-        minTime = params.minTime || 0,
-        baseUrl = params.baseUrl || '',
-        len = srcArr.length,
-        t = minTime / len,
-        st = (new Date()).getTime()
-
-    for (let i = 0; i < len; i++) {
-        (function (i) {
-            if (typeof (srcArr[i]) == 'string') srcArr[i] = { path: srcArr[i], name: i }
-            let newImg = new Image()
-            if (srcArr[i].crossOrigin) newImg.crossOrigin = srcArr[i].crossOrigin
-            newImg.onload = newImg.onerror = function (e) {
-                e = e || window.event
-                let self = this
-                endLoad(self, e.type, i)
-            }
-            setTimeout(function () {
-                newImg.src = baseUrl + srcArr[i].path
-            }, t * (i + 1) - ((new Date()).getTime() - st))
-        }(i))
-    }
-    function endLoad(this_, eType, i) {
-        num++
-        let progress = num / len
-        srcArr[i]['result'] = this_
-        srcArr[i]['progress'] = progress
-        srcArr[i]['index'] = i
-        srcArr[i]['status'] = eType == 'load' ? 200 : 'Failed to load'
-        imgArrObj[srcArr[i].name] = this_
-        params.fileload && params.fileload(srcArr[i])
-        if (num === len) params.complete && params.complete(imgArrObj)
-    }
-}
-// 
-export function lazyLoad(selector, params) {
-    params = params || {}
-    let doc = document,
-        assets = [],
-        ele = doc.querySelectorAll(selector),
-        baseUrl = params.baseUrl || ''
-
-    for (let i = 0, len = ele.length; i < len; i++) {
-        let obj = { path: '', type: '', ele: ele[i], name: '_' + i, crossOrigin: null }
-        if (ele[i].nodeName === 'IMG') {
-            obj.type = 'img'
-        } else {
-            obj.type = 'bj'
-        }
-        obj.path = ele[i].getAttribute('data-src')
-        obj.crossOrigin = ele[i].getAttribute('crossOrigin')
-        // 过滤已转为base64的图片
-        let result = /^data:([^;]+);base64,/gmi
-        if (result.test(obj.path)) {
-            if (obj.type == 'img') {
-                obj.ele.setAttribute('src', obj.path)
-            } else if (obj.type == 'bj') {
-                obj.ele.style.backgroundImage = 'url(' + obj.path + ')'
-            }
-            continue
-        }
-        if (obj.path) {
-            assets.push(obj)
-        }
-    };
-    preload(assets, {
-        fileload: function (item) {
-            if (item.status === 200) {
-                if (item.type == 'img') {
-                    item.ele.setAttribute('src', baseUrl + item.path)
-                } else if (item.type == 'bj') {
-                    item.ele.style.backgroundImage = 'url(' + baseUrl + item.path + ')'
-                }
-            }
-            if (params.fileload) params.fileload(item)
-        },
-        complete: function (result) {
-            if (params.complete) params.complete(result)
-        },
-        minTime: params.minTime,
-        baseUrl: baseUrl
-    })
-}
-
 // 获取地址参数
 export function queryString(name) {
     let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)')
@@ -441,4 +350,9 @@ export function bindFileControl(btnEle, accept, opt) {
         fileEle.click()
     }
     return fileEle
+}
+
+// 延迟
+export function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
--- template-react/js/context.js
+++ template-react/js/context.js
@@ -1,3 +1,3 @@
 import { createContext } from 'react'
 
-export const numClick = createContext(0)
+export const numClick = createContext()
